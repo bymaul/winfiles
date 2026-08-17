4b5e75035eb26bc15040b031d5c6538ec4532a29
Maulana Ahmad Aji Triadi <maulanaatriadi@gmail.com>
2026-08-17 12:11:23 +0700

    refactor(powershell): organize profile, remove undefined gad alias

--- windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
+# ---------------------------------------------------------------------------
 # Aliases
+# ---------------------------------------------------------------------------
+
 Set-Alias -Name vim -Value nvim
 Set-Alias -Name open -Value explorer
 Set-Alias -Name ls -Value Get-ChildItemFormatted
@@ -8,7 +11,6 @@ Set-Alias -Name cat -Value bat
 Set-Alias -Name which -Value Get-CommandDefinition
 Set-Alias -Name nah -Value Reset-GitChanges
 Set-Alias -Name glog -Value Get-GitLog
-Set-Alias -Name gad -Value Export-GitDiffArchive
 Set-Alias -Name art -Value Invoke-LaravelArtisan
 Set-Alias -Name us -Value Update-SystemSoftware
 Set-Alias -Name lg -Value lazygit
@@ -17,187 +19,127 @@ Set-Alias -Name opencode -Value Invoke-OpenCodeWSL
 Set-Alias -Name su -Value Update-ShellElevation
 Set-Alias -Name oc -Value opencode
 
-# Functions
-function Get-ChildItemFormatted
-{
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $false, Position = 0)]
-		[string]$Path = $PWD
-	)
+# ---------------------------------------------------------------------------
+# File operations
+# ---------------------------------------------------------------------------
 
-	eza -a -l --header --icons --hyperlink --time-style relative $Path
+function Get-ChildItemFormatted {
+    param([string]$Path = $PWD)
+    eza -a -l --header --icons --hyperlink --time-style relative $Path
+}
+
+function New-File {
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [string[]]$Path
+    )
+    process {
+        foreach ($p in $Path) {
+            if (Test-Path $p) { (Get-Item $p).LastWriteTime = Get-Date }
+            else { New-Item -ItemType File -Path $p | Out-Null }
+        }
+    }
+}
+
+function Remove-ItemForceRecursive {
+    param([Parameter(Mandatory)][string]$Path)
+    Remove-Item -Path $Path -Recurse -Force
+}
+
+# ---------------------------------------------------------------------------
+# Search
+# ---------------------------------------------------------------------------
+
+function Find-String {
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Pattern,
+
+        [Parameter(Position = 1, ParameterSetName = 'FileSearch')]
+        [string[]]$Path,
+
+        [Parameter(ParameterSetName = 'FileSearch')]
+        [switch]$Recurse,
+
+        [switch]$IgnoreCase,
+
+        [Parameter(ValueFromPipeline)]
+        [AllowEmptyString()]
+        [string]$InputObject
+    )
+
+    begin {
+        $input_ = [System.Collections.ArrayList]::new()
+        $params = @{ Pattern = $Pattern; AllMatches = $true }
+        if ($IgnoreCase) { $params['CaseSensitive'] = $false }
+    }
+
+    process { if ($InputObject) { $null = $input_.Add($InputObject) } }
+
+    end {
+        if ($input_.Count -gt 0) {
+            $input_ | Select-String @params
+            return
+        }
+        $childParams = @{ Path = $Path }
+        if ($Recurse) { $childParams['Recurse'] = $true }
+        Get-ChildItem @childParams |
+            Where-Object { $_ -is [System.IO.FileInfo] } |
+            Select-String @params
+    }
+}
+
+# ---------------------------------------------------------------------------
+# Git
+# ---------------------------------------------------------------------------
+
+function Reset-GitChanges {
+    git reset --hard
+    git clean -df
+}
+
+function Get-GitLog {
+    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
+# ---------------------------------------------------------------------------
+# Dev tools
+# ---------------------------------------------------------------------------
+
+function Invoke-LaravelArtisan { php artisan @args }
+
+function Invoke-OpenCodeWSL { wsl zsh -ic "opencode $($args)" }
+
+# ---------------------------------------------------------------------------
+# System
+# ---------------------------------------------------------------------------
+
+function Get-CommandDefinition {
+    param([Parameter(Mandatory)][string]$Name)
+    Get-Command $Name | Select-Object -ExpandProperty Definition
 }
 
 function Update-ShellElevation {
     sudo -E pwsh -NoLogo -Interactive -NoExit -c "Clear-Host"
 }
 
-function Find-String
-{
-	[CmdletBinding(DefaultParameterSetName='Default')]
-	param(
-		[Parameter(Mandatory=$true, Position=0)]
-		[string]$Pattern,
-
-		[Parameter(Mandatory=$false, Position=1, ParameterSetName='FileSearch')]
-		[string[]]$Path,
-
-		[Parameter(Mandatory=$false, ParameterSetName='FileSearch')]
-		[switch]$Recurse,
-
-		[Parameter(Mandatory=$false)]
-		[switch]$IgnoreCase,
-
-		[Parameter(Mandatory=$false, ValueFromPipeline=$true)]
-		[AllowEmptyString()]
-		[string]$InputObject
-	)
-
-	begin
-	{
-		$pipelineInput = [System.Collections.ArrayList]::new()
-
-		$selectStringParams = @{
-			Pattern = $Pattern
-			AllMatches = $true
-		}
-
-		if ($IgnoreCase)
-		{
-			$selectStringParams['CaseSensitive'] = $false
-		}
-	}
-
-	process
-	{
-		if ($InputObject)
-		{
-			$null = $pipelineInput.Add($InputObject)
-		}
-	}
-
-	end
-	{
-		try
-		{
-			if ($pipelineInput.Count -gt 0)
-			{
-				$pipelineInput | Select-String @selectStringParams
-				return
-			}
-
-			if ($Path)
-			{
-				$searchParams = @{
-					Path = $Path
-				}
-
-				if ($Recurse)
-				{
-					$searchParams['Recurse'] = $true
-				}
-
-				Get-ChildItem @searchParams | 
-					Where-Object { $_ -is [System.IO.FileInfo] } | 
-					Select-String @selectStringParams
-			} else
-			{
-				Get-ChildItem | 
-					Where-Object { $_ -is [System.IO.FileInfo] } | 
-					Select-String @selectStringParams
-			}
-		} catch
-		{
-			Write-Error "An error occurred while searching: $_"
-		}
-	}
+function Update-SystemSoftware {
+    sudo winget upgrade --all --include-unknown --silent --verbose
+    winget upgrade --all --include-unknown --silent --verbose
+    sudo choco upgrade all -y
 }
 
-function New-File
-{
-	[CmdletBinding()]
-	param(
-		[Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
-		[string[]]$Path
-	)
+# ---------------------------------------------------------------------------
+# Prompt
+# ---------------------------------------------------------------------------
 
-	process
-	{
-		foreach ($filePath in $Path)
-		{
-			if (Test-Path $filePath)
-			{
-				(Get-Item $filePath).LastWriteTime = Get-Date
-			} else
-			{
-				New-Item -ItemType File -Path $filePath | Out-Null
-			}
-		}
-	}
-}
+function Invoke-Starship-TransientFunction { &starship module character }
 
-function Update-SystemSoftware
-{
-	Write-Verbose "Updating software installed via Winget & Chocolatey"
+# ---------------------------------------------------------------------------
+# Shell integration
+# ---------------------------------------------------------------------------
 
-	sudo winget upgrade --all --include-unknown --silent --verbose
-	# upgrade all packages installed without superuser
-	winget upgrade --all --include-unknown --silent --verbose
-	sudo choco upgrade all -y
-}
-
-function Get-CommandDefinition
-{
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true, Position = 0)]
-		[string]$Name
-	)
-
-	Write-Verbose "Retrieving definition for command '$Name'"
-	Get-Command $Name | Select-Object -ExpandProperty Definition
-}
-
-function Invoke-LaravelArtisan
-{
-	php artisan @args
-}
-
-function Remove-ItemForceRecursive
-{
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true, Position = 0)]
-		[string]$Path
-	)
-
-	Write-Verbose "Removing item '$Path' recursively and forcefully"
-	Remove-Item -Path $Path -Recurse -Force
-}
-
-function Invoke-OpenCodeWSL
-{
-	wsl zsh -ic "opencode $($args)"
-}
-
-function Reset-GitChanges
-{
-	git reset --hard
-	git clean -df
-}
-
-function Get-GitLog
-{
-	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-}
-
-function Invoke-Starship-TransientFunction
-{
-	&starship module character
-}
-
-# Shell Integration
 Invoke-Expression (&starship init powershell)
 Enable-TransientPrompt
 
